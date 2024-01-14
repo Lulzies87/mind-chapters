b3f2b743811becd4fafdff6cfe416730a2b1765a
Lilach E <lilachelayza@gmail.com>
2024-01-14 10:47:44 +0200

    save chapter from form to DB

--- src/chapters.model.ts
+++ src/chapters.model.ts
@@ -1,6 +1,17 @@
+import { Schema, model } from "mongoose"
 interface Chapter {
     author: string;
     title: string;
     content: string;
     timePosted: Date;
 }
+
+const schema = new Schema<Chapter>({
+    author: { type: String, required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    timePosted: { type: Date, immutable: true, required: true }
+    }
+);
+
+export const Chapter = model<Chapter>("Chapter", schema, "chapters");
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,35 @@ import { createServer } from "http";
 import express from "express";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import { Chapter } from "./chapters.model";
 
 const app = express();
 
 app.use(json());
 
-app.get("/api/hello", (req, res) => {
-    res.send("world");
+app.post("/api/chapters", async (req, res, next) => {
+  const { author, title, content, timePosted } = req.body;
+
+  try {
+    if (!title || !content) {
+      res.status(400);
+      res.send("Must provide title and content");
+      return;
+    }
+
+    await Chapter.create({
+      author,
+      title,
+      content,
+      timePosted,
+    });
+
+    res.status(201);
+    res.end();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 app.use(express.static("public"));
@@ -18,13 +40,14 @@ const server = createServer(app);
 const port = process.env.PORT ?? 3000;
 
 async function startServer() {
-    if (!process.env.CONN_STRING) {
-        throw new Error ("Must provide a connection string");
-    }
+  if (!process.env.CONN_STRING) {
+    throw new Error("Must provide a connection string");
+  }
 
-    await mongoose.connect(process.env.CONN_STRING, {
-        dbName: "mind-chapters"
-    });
+  await mongoose.connect(process.env.CONN_STRING, {
+    dbName: "mind-chapters",
+  });
 }
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
+startServer();
